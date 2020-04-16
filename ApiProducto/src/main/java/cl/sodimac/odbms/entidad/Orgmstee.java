701a542ef46a577ca6ebf28f579b2699c6603b12
gaguajardo <gaguajardo@sodimac.cl>
2020-04-16 16:42:13 -0400

    Clase de Tiendas

--- ApiProducto/src/main/java/cl/sodimac/odbms/entidad/Orgmstee.java
+++ ApiProducto/src/main/java/cl/sodimac/odbms/entidad/Orgmstee.java
@@ -5,11 +5,13 @@ public class Orgmstee {
 	
 	private Integer org_lvl_child;
 	private String  org_name_full;
+	private Integer org_lvl_number;
 	
-	public Orgmstee (Integer org_lvl_child, String org_name_full) {
+	public Orgmstee (Integer org_lvl_child, String org_name_full, Integer org_lvl_number) {
 		super();
 		this.org_lvl_child = org_lvl_child;
 		this.org_name_full = org_name_full;
+		this.org_lvl_number = org_lvl_number;
 		
 		
 	}
