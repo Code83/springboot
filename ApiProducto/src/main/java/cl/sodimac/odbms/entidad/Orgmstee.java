8f44344865d6459461bed1f8a9cedf5dbad786d3
gaguajardo <gaguajardo@sodimac.cl>
2020-04-16 16:31:33 -0400

    Se agrega al pkg entidad, clase de orgmstee

---
+++ ApiProducto/src/main/java/cl/sodimac/odbms/entidad/Orgmstee.java
@@ -0,0 +1,16 @@
+package cl.sodimac.odbms.entidad;
+
+public class Orgmstee {
+
+	
+	private Integer org_lvl_child;
+	private String  org_name_full;
+	
+	public Orgmstee (Integer org_lvl_child, String org_name_full) {
+		super();
+		this.org_lvl_child = org_lvl_child;
+		this.org_name_full = org_name_full;
+		
+		
+	}
+}
